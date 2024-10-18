64dd09acca6c0996131190605ee35b9355dc4b4a
mariaisva2 <mariac20160@gmail>
2024-10-18 06:59:41 -0500

    Change navbar

--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link'
 import React from 'react'
 import { NavBarComponent } from './nav-bar.component'
 
@@ -6,12 +5,7 @@ export const HeaderComponent = () => {
   return (
     <header className="bg-gray-900 text-white">
     <div className="container mx-auto flex justify-between items-center py-4 px-6">
-      {/* Logo */}
-      <div className="text-2xl font-bold">
-        <Link href="/" className="hover:text-gray-300">Mi Portafolio</Link>
-      </div>
-
-      {/* Navegación */}
+    
       <NavBarComponent />
      
     </div>
